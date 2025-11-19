75e3e8c1ff9445b89b5d827954bf6a8428076cce
iqbalmudzakky <iqbalmudzakky@gmail.com>
2025-11-20 00:22:55 +0700

    benerin store redux

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-// import chatReducer from "./slices/chatSlice";
+import chatReducer from "./slices/chatSlice";
 import userReducer from "./slices/userSlice";
 import roomReducer from "./slices/roomSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    // chat: chatReducer,
+    chat: chatReducer,
     user: userReducer,
     room: roomReducer,
   },
